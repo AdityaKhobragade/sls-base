b7c9bcfabb1ec3e5f71e3dbfb3d25aba56abe45f
Aditya <97386862+AdityaKhobragade@users.noreply.github.com>
2025-03-30 23:33:37 +0530

    addedcode fordynamoDB

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,14 @@
-// src/handlers/createAuction.js
+import { v4 as uuid } from 'uuid'
+import AWS from 'aws-sdk'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction( event, context ) {
   const { title } = JSON.parse( event.body );
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'open',  // Make sure 'open' is a string
     createdAt: now.toISOString(),
@@ -12,6 +16,10 @@ async function createAuction( event, context ) {
 
   console.log( "***auction = ", auction );
 
+  await dynamodb.put( {
+    TableName: 'AuctionTable',
+    Item: auction
+  } ).promise();
 
   return {
     statusCode: 201,
